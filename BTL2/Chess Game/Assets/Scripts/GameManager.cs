ee3a59fe19f3a707f621e46dddcb8d7e1472981d
phuonghoang20 <phuonghoanghai0711@gmail.com>
2024-05-09 16:35:49 +0700

    BTL2

---
+++ BTL2/Chess Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    AlphaBeta ab = new AlphaBeta();
+    private bool _kingDead = false;
+    float timer = 0;
+    Board _board;
+	void Start ()
+    {
+        _board = Board.Instance;
+        _board.SetupBoard();
+	}
+
+	void Update ()
+    {
+        if (_kingDead)
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (!playerTurn && timer < 1)
+        {
+            timer += Time.deltaTime;
+        }
+        else if (!playerTurn && timer >= 1)
+        {
+            Move move = ab.GetMove();
+            _DoAIMove(move);
+            timer = 0;
+        }
+	}
+
+    public bool playerTurn = true;
+
+    void _DoAIMove(Move move)
+    {
+        Tile firstPosition = move.firstPosition;
+        Tile secondPosition = move.secondPosition;
+
+        if (secondPosition.CurrentPiece && secondPosition.CurrentPiece.Type == Piece.pieceType.KING)
+        {
+            SwapPieces(move);
+            _kingDead = true;
+        }
+        else
+        {
+            SwapPieces(move);
+        }
+    }
+
+    public void SwapPieces(Move move)
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Highlight");
+        foreach (GameObject o in objects)
+        {
+            Destroy(o);
+        }
+
+        Tile firstTile = move.firstPosition;
+        Tile secondTile = move.secondPosition;
+
+        firstTile.CurrentPiece.MovePiece(new Vector3(-move.secondPosition.Position.x, 0, move.secondPosition.Position.y));
+
+        if (secondTile.CurrentPiece != null)
+        {
+            if (secondTile.CurrentPiece.Type == Piece.pieceType.KING)
+                _kingDead = true;
+            Destroy(secondTile.CurrentPiece.gameObject);
+        }
+            
+
+        secondTile.CurrentPiece = move.pieceMoved;
+        firstTile.CurrentPiece = null;
+        secondTile.CurrentPiece.position = secondTile.Position;
+        secondTile.CurrentPiece.HasMoved = true;
+
+        playerTurn = !playerTurn;
+    }
+}
